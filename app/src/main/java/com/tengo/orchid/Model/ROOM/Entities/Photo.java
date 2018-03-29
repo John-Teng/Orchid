0fa37a5b223e6f726852877b75cfa138de9676c2
John Teng <john.teng97@gmail.com>
2018-03-28 22:46:52 -0400

    Changed sdk version to be compatible with room, fixed room compilation error

--- app/src/main/java/com/tengo/orchid/Model/ROOM/Entities/Photo.java
+++ app/src/main/java/com/tengo/orchid/Model/ROOM/Entities/Photo.java
@@ -10,13 +10,13 @@ import android.arch.persistence.room.PrimaryKey;
 @Entity
 public class Photo {
     @PrimaryKey
-    public final int photoId;
+    public final int id;
     public final int beautyRating;
     public final int age;
     public final String path;
 
     public Photo(int id, int beautyRating, int age, String path) {
-        this.photoId = id;
+        this.id = id;
         this.beautyRating = beautyRating;
         this.age = age;
         this.path = path;
